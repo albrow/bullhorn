8b0b5d10f0ce4c0858a94d15707e5186e7ce355a
albrow <stephenalexbrowne@gmail.com>
2014-03-30 13:32:30 -0400

    Add styles to the new broadcasts page

--- app/controllers/broadcasts.go
+++ app/controllers/broadcasts.go
@@ -4,6 +4,7 @@ import (
 	"bullhorn/app/models"
 	"github.com/albrow/zoom"
 	"github.com/revel/revel"
+	"github.com/sfreiberg/gotwilio"
 )
 
 type Broadcasts struct {
@@ -22,22 +23,44 @@ func (c Broadcasts) Create(body string, voice bool, sms bool) revel.Result {
 		return c.Redirect(Broadcasts.New)
 	}
 
-	if sms {
-		revel.INFO.Println("SENDING...")
-		revel.INFO.Println("MESSAGE: ", body)
-		subscribers := make([]*models.SMSSubscriber, 0)
-		if err := zoom.NewQuery("Subscriber").Include("Phone").Scan(&subscribers); err != nil {
+	revel.INFO.Println("SENDING...")
+	revel.INFO.Println("MESSAGE: ", body)
+
+	// SMS
+	smsSubs := make([]*models.SMSSubscriber, 0)
+	if err := zoom.NewQuery("SMSSubscriber").Include("Phone").Scan(&smsSubs); err != nil {
+		return c.RenderError(err)
+	}
+	revel.INFO.Println(len(smsSubs), "SUBSCRIBERS")
+	for _, s := range smsSubs {
+		revel.INFO.Println("TO: ", s.Phone)
+		_, _, err := twilio.SendSMS("+19103780902", s.Phone, body, "", "")
+		if err != nil {
+			revel.ERROR.Println(err)
 			return c.RenderError(err)
 		}
-		revel.INFO.Println(len(subscribers), "SUBSCRIBERS")
-		for _, s := range subscribers {
-			revel.INFO.Println("TO: ", s.Phone)
-			_, _, err := twilio.SendSMS("+19103780902", s.Phone, body, "", "")
-			if err != nil {
-				return c.RenderError(err)
-			}
+	}
+
+	// VOICE
+	voiceSubs := make([]*models.VoiceSubscriber, 0)
+	if err := zoom.NewQuery("VoiceSubscriber").Include("Phone").Scan(&voiceSubs); err != nil {
+		return c.RenderError(err)
+	}
+	revel.INFO.Println(len(voiceSubs), "SUBSCRIBERS")
+	for _, s := range voiceSubs {
+		revel.INFO.Println("TO: ", s.Phone)
+		callbackParams := gotwilio.NewCallbackParameters("http://107.170.105.233/say/" + body)
+		_, _, err := twilio.CallWithUrlCallbacks("+19103780902", s.Phone, callbackParams)
+		if err != nil {
+			revel.ERROR.Println(err)
+			return c.RenderError(err)
 		}
 	}
+
 	c.Flash.Success("Your Message Was Sent")
 	return c.Redirect(Broadcasts.New)
+}
+
+func (c Broadcasts) Say(message string) revel.Result {
+	return c.Render(message)
 }
